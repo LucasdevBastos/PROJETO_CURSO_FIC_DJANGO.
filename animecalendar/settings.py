39210acbf69fcb23436c0ab8eb94c2e30516cd8c
Lucaason <lucasviniciusdesousabastos@gmail.com>
2025-12-12 07:37:52 -0300

    Auto-create superuser on deploy in settings.py
    Added logic to automatically create a Django superuser during deployment if environment variables for username, email, and password are set and the user does not already exist. This streamlines initial setup and deployment processes.

--- animecalendar/settings.py
+++ animecalendar/settings.py
@@ -16,6 +16,20 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
 DEBUG = True
 
+# Auto create superuser on deploy
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if os.environ.get("DJANGO_SUPERUSER_USERNAME") and not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
+    User.objects.create_superuser(
+        username=os.environ["DJANGO_SUPERUSER_USERNAME"],
+        email=os.environ.get("DJANGO_SUPERUSER_EMAIL"),
+        password=os.environ["DJANGO_SUPERUSER_PASSWORD"]
+    )
+    print("Superuser created.")
+
+
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
